1c0a7ec0520a67457ff0916699734364acb2277c
kkauraiims <89849204+kkauraiims@users.noreply.github.com>
2023-04-04 05:48:49 +0530

    Update ve_compute_hurst.m

--- ve_compute_hurst.m
+++ ve_compute_hurst.m
@@ -4,9 +4,9 @@ function ve_compute_hurst (patient_id)
 % no manual input required 
 % input files- ve time series in .dat format
 
-% KK, October, 2022
+% KK, October, 2022, updated December, 2022
 
-addpath '/Users/neelbazro/Desktop/HE'
+addpath '/Users/neelbazro/Desktop/HE/hurst_scripts'
  
 patient_dir = strcat ('/Users/neelbazro/Desktop/he_db/output','/', patient_id, '/time_series');
 cd (patient_dir)
@@ -14,13 +14,25 @@ cd (patient_dir)
 %% load virtual electrode time_series 
 ve_files = dir ('*.dat');
 
+%% Create a directory for storing the Hurst output 
+mkdir(strcat('/Users/neelbazro/Desktop/he_db/output','/', patient_id, '/H'))
+
+%% run the hurst_mod script and save the output to a text file
+
+tic
  for i = 1: length (ve_files)
      ve = load (ve_files(i).name);
-     [d,RSe,H,a] = hurst_mod(ve(2,:),50); % use hurst_mod function to calculate H 
-     fname = ['/Users/neelbazro/Desktop/he_db/output/' patient_id, '/H/', 'H_' num2str(i) , '.txt']; 
-     save (fname, 'H', '-ascii')
+     [~,~,H,~] = hurst_mod(ve(2,:),50); % use hurst_mod function to calculate H 
+     H_idx = extractBetween(ve_files(i).name, "_", ".dat"); % extract the index of the time series 
+     % create a file name including the index of the ve time series for
+     % which the Hurst Exponent was derived
+     fname = ['/Users/neelbazro/Desktop/he_db/output/' patient_id, '/H/', 'H_', H_idx{1,1} , '.txt']; 
+     % save H of every time series as a separate .txt file with appended ve number
+     save (fname, 'H', '-ascii') 
      close all
+     clear ve % clean variable data to save computer memory
  end 
+ toc
      
      
      
